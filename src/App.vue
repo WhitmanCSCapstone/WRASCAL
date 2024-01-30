d1f0a92770c2ac81939e3c7da9db08a3c9b0287c
Jack Allard <jackallard17@gmail.com>
2024-01-30 09:20:31 -0800

    added lukes files

--- src/App.vue
+++ src/App.vue
@@ -87,10 +87,8 @@
           protons and various metal ions. Over 18,000 papers have been examined,
           representing over 99,000 metal-ligand systems, and only data which meet
           the criterion of well-documented, careful work were considered for
-          inclusion.
-        </div>
-
         <v-divider/>
+        </div>
 
         <div>
           {{ new Date().getFullYear() }} — <strong>NIST-46</strong>
@@ -98,14 +96,19 @@
       </v-footer>
     </v-main>
   </v-app>
+  <login-dialog v-model="loginDialogVisible"></login-dialog>
 </template>
 
 <script lang="ts">
 
 import { defineComponent } from 'vue'
 import { useTheme } from 'vuetify'
+import Login from './views/Login.vue'
 
 export default defineComponent({
+  components: {
+    Login
+  },
   setup () {
     const theme = useTheme()
     const isDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
@@ -130,9 +133,15 @@ export default defineComponent({
         name: 'About Us',
         to: '/about-us',
         icon: 'mdi-information'
+      },
+      {
+        name: 'Test',
+        to: '/new-entry',
+        icon: 'mdi-plus'
       }
     ],
-    drawer: false
+    drawer: false,
+    loginDialogVisible: false
   }),
   computed: {
     themeModeSwitchIcon(){
@@ -142,8 +151,8 @@ export default defineComponent({
     }
   },
   methods: {
-    login(){
-      this.$router.push('/login')
+    login() {
+      this.loginDialogVisible = true;
     },
 
     changeDrawerStatus(){
