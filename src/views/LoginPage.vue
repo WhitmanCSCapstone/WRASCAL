b697b90b200c5ef2f531233e49bf78c1d97ebd7a
Jack Allard <jackallard17@gmail.com>
2024-02-08 15:53:06 -0800

    implement password sign in

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,65 +1,97 @@
-
 <template>
-    <v-card class="mx-auto px-6 py-8" max-width="344">
-      <v-form
-        v-model="form"
-        @submit.prevent="onSubmit"
+  <v-container class="pt-8">
+    <h1> Log In</h1><br>
+    <v-row class="pt-8">
+      <v-col
+        cols="12"
+        md="12"
       >
         <v-text-field
-          v-model="email"
-          :readonly="loading"
-          :rules="[required]"
-          class="mb-2"
-          clearable
           label="Email"
+          prepend-icon="mdi-list-box-outline"
+          variant="solo"
+          :loading="isLoading ?? false"
+          v-model:model-value="emailValue"
         ></v-text-field>
+      </v-col>
 
+      <v-col
+        cols="12"
+        md="12"
+      >
         <v-text-field
-          v-model="password"
-          type="password"
-          :readonly="loading"
-          :rules="[required]"
-          clearable
           label="Password"
-          placeholder="Enter your password"
+          prepend-icon="mdi-lock"
+          variant="solo"
+          :loading="isLoading ?? false"
+          v-model:model-value="passwordValue"
+          type="password"
         ></v-text-field>
+      </v-col>
 
-        <br>
+      <v-col
+        cols="12"
+        md="6"
+      >
+      </v-col>
 
-        <v-btn
-          :disabled="!form"
-          :loading="loading"
-          block
-          color="primary"
-          size="large"
-          type="submit"
-          variant="elevated"
-        >
-          Sign In
-        </v-btn>
-      </v-form>
-    </v-card>
+      <v-col
+        cols="12"
+        md="6"
+      >
+      </v-col>
+    </v-row>
+
+    <v-btn type="submit" block class="mt-2" color="primary" @click="submitForm">Submit</v-btn>
+  </v-container>
 </template>
-<script>
-  export default {
-    data: () => ({
-      form: false,
-      email: null,
-      password: null,
-      loading: false,
-    }),
 
-    methods: {
-      onSubmit () {
-        if (!this.form) return
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { SupabaseClient } from '@supabase/supabase-js';
 
-        this.loading = true
-
-        setTimeout(() => (this.loading = false), 2000)
-      },
-      required (v) {
-        return !!v || 'Field is required'
-      },
+export default defineComponent({
+  name: "NewEntryForm",
+  props: {
+    isLoading: {
+      type: Boolean,
+      default: false
     },
+    email: {
+      type: String,
+      default:''
+    },
+    formula: {
+      type: String,
+      default: ''
+    },
+    metalCharge: {
+      type: String,
+      default: ''
+    },
+    ligandCharge: {
+      type: String,
+      default: ''
+    }
+  },
+  data: () => ({
+    emailValue: '',
+    passwordValue: '',
+    ligandChargeValue: ''
+  }),
+  methods: {
+    async submitForm() {
+      const supabase = new SupabaseClient('https://eauyarvlibdxezijtoyx.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhdXlhcnZsaWJkeGV6aWp0b3l4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE0NjQ3NDcsImV4cCI6MjAxNzA0MDc0N30.3u320_sLG2xIyXRRVs4_TyO44w9kc0TJnhaLja5JyAA')
+      try {
+        const { error } = await supabase.auth.signInWithPassword({
+          email: this.emailValue,
+          password: this.passwordValue,
+        });
+        if (error) throw error;
+      } catch (error) {
+          console.log('Error: ', error)
+      }
+    }
   }
+})
 </script>
