248938b55d425897f8517e3c25e90d6eea652adb
Jack Allard <jackallard17@gmail.com>
2024-02-07 19:08:26 -0800

    bump port number

--- src/api/dataInjection.js
+++ src/api/dataInjection.js
@@ -57,7 +57,7 @@ app.post('/inject/condition', async (req, res) => {
 });
 
 // Start the Express server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
