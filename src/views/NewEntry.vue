42b6fad8f1251952b70a777db1a22adb10fe740e
looko <samueluk@uw.edu>
2024-02-19 23:49:25 -0800

    has all the fields now but seriously there had to be a better way to do this

--- src/views/NewEntry.vue
+++ src/views/NewEntry.vue
@@ -3,49 +3,21 @@ import Dropdown from 'primevue/dropdown';
 <template>
   <v-container class="pt-8">
     <h1> New Ligand Submission </h1><br>
-    <!-- <v-alert
-      border="start"
-      variant="tonal"
-      type="info"
-      closable
-      close-label="Close Alert"
-      title="Advance Search Tips"
-      class="text-left"
-    >
-      All the fields below support multiple entries, you can input one or more entries split by: <b>,</b>
-      For example: Fe, H, Ca
-    </v-alert> -->
-
+    <h2> Metal Information </h2>
     <v-row class="pt-8">
       <v-col
-        cols="6"
-        md="12"
+        cols="12"
+        md="6"
       >
         <v-text-field
           label="Metal"
           prepend-icon="mdi-gold"
           variant="solo"
           :loading="isLoading ?? false"
-          @input="onLigandNameUpdate"
-          v-model:model-value="ligandNameValue"
+          @input="onMetalNameUpdate"
+          v-model:model-value="metalNameValue"
         ></v-text-field>
       </v-col>
-
-      <v-col
-        cols="12"
-        md="12"
-      >
-        <!-- TODO: FIGURE OUT HOW TO ENTER CHEMICAL DATA!!!-->
-        <v-text-field
-          label="Formula"
-          prepend-icon="mdi-flask"
-          variant="solo"
-          :loading="isLoading ?? false"
-          @input="onFormulaUpdate"
-          v-model:model-value="formulaValue"
-        ></v-text-field>
-      </v-col>
-
       <v-col
         cols="12"
         md="6"
@@ -59,19 +31,170 @@ import Dropdown from 'primevue/dropdown';
           v-model:model-value="metalChargeValue"
         ></v-text-field>
       </v-col>
+    </v-row>
+
+    <h2> Ligand Information</h2>
+    <v-row class="pt-8">
+      <v-col
+        cols="12"
+        md="12"
+      >
+        <v-text-field
+          label="Name"
+          prepend-icon="mdi-pen"
+          variant="solo"
+          :loading="isLoading ?? false"
+          @input="onLigandNameUpdate"
+          v-model:model-value="ligandNameValue"
+        ></v-text-field>
+      </v-col><br>
+
+      <v-col
+        cols="12"
+        md="4"
+      >
+        <!-- TODO: FIGURE OUT HOW TO ENTER CHEMICAL DATA!!!-->
+        <v-text-field
+          label="Formula"
+          prepend-icon="mdi-flask"
+          variant="solo"
+          :loading="isLoading ?? false"
+          @input="onLigandFormulaUpdate"
+          v-model:model-value="ligandFormulaValue"
+        ></v-text-field>
+      </v-col>
+      <v-col
+        cols="12"
+        md="4"
+      >
+        <v-text-field
+          label=".mol File (TODO FIX)"
+          prepend-icon="mdi-file-cabinet"
+          variant="solo"
+          :loading="isLoading ?? false"
+          @input="onLigandFileUpdate"
+          v-model:model-value="ligandFileValue"
+        ></v-text-field>
+      </v-col>
+      <v-col
+        cols="12"
+        md="4"
+      >
+        <v-text-field
+          label="Protonation Level"
+          prepend-icon="mdi-atom-variant"
+          variant="solo"
+          :loading="isLoading ?? false"
+          @input="onProtonationLevelUpdate"
+          v-model:model-value="protonationLevelValue"
+        ></v-text-field>
+      </v-col>
 
       <v-col
         cols="12"
         md="6"
       >
         <v-text-field
-          label="Ligand Charge"
-          prepend-icon="mdi-lightning-bolt-outline"
+          label="Constant Type"
+          prepend-icon="mdi-atom-variant"
           variant="solo"
           :loading="isLoading ?? false"
-          @input="onLigandChargeUpdate"
-          v-model:model-value="ligandChargeValue"
+          @input="onConstantTypeUpdate"
+          v-model:model-value="constantTypeValue"
         ></v-text-field>
+      </v-col>
+      <v-col
+        cols="12"
+        md="6"
+      >
+        <v-text-field
+          label="Equilibrium Expression"
+          prepend-icon="mdi-atom-variant"
+          variant="solo"
+          :loading="isLoading ?? false"
+          @input="onEquilibriumExpressionUpdate"
+          v-model:model-value="equilibriumExpressionValue"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+
+    <h2> Conditions </h2>
+    <v-row class="pt-8">
+      <v-col
+        cols="12"
+        md="6"
+      >
+        <v-text-field
+          label="Temperature"
+          prepend-icon="mdi-atom-variant"
+          variant="solo"
+          :loading="isLoading ?? false"
+          @input="onTemperatureUpdate"
+          v-model:model-value="temperatureValue"
+        ></v-text-field>
+
+        <v-text-field
+          label="Ionic Strength"
+          prepend-icon="mdi-atom-variant"
+          variant="solo"
+          :loading="isLoading ?? false"
+          @input="onIonicStrengthUpdate"
+          v-model:model-value="ionicStrengthValue"
+        ></v-text-field>
+      </v-col>
+
+      <v-col
+        cols="12"
+        md="6"
+      >
+        <v-text-field
+          label="Footnote"
+          prepend-icon="mdi-atom-variant"
+          variant="solo"
+          :loading="isLoading ?? false"
+          @input="onFootnoteUpdate"
+          v-model:model-value="footnoteValue"
+        ></v-text-field>
+
+        <v-text-field
+            label="Value"
+            prepend-icon="mdi-atom-variant"
+            variant="solo"
+            :loading="isLoading ?? false"
+            @input="onValueUpdate"
+            v-model:model-value="valueValue"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+
+    <h2> Reference Information </h2>
+    <v-row class="pt-8">
+      <v-col
+        cols="12"
+        md="6"
+      >
+        <v-text-field
+          label="Entry Code"
+          prepend-icon="mdi-atom-variant"
+          variant="solo"
+          :loading="isLoading ?? false"
+          @input="onEntryCodeUpdate"
+          v-model:model-value="entryCodeValue"
+        ></v-text-field>
+      </v-col>
+      <v-col
+        cols="12"
+        md="6"
+      >
+        <v-text-field
+          label="DOI"
+          prepend-icon="mdi-atom-variant"
+          variant="solo"
+          :loading="isLoading ?? false"
+          @input="onDOIupdate"
+          v-model:model-value="DOIValue"
+        ></v-text-field>
+  
       </v-col>
     </v-row>
 
@@ -83,15 +206,25 @@ import Dropdown from 'primevue/dropdown';
 import { defineComponent } from 'vue'
 
 interface dataEntry {
-  ligandName: string,
-  formula: string,
-  metalCharge: string,
-  ligandCharge: string
+  metalName: String,
+  metalCharge: String,
+  ligandName: String,
+  ligandFormua: String,
+  ligandFile: String,
+  protonationLevel: String,
+  constantType: String,
+  equilibriumExpression: String,
+  temperature: String,
+  ionicStrength: String,
+  footnote: String,
+  value: String,
+  referenceEntryCode: String,
+  referenceDOIValue: String
 }
 
 async function postJSON(data: dataEntry) {
   try {
-    const response = await fetch("<YOUR ENDPOINT HERE!!!>", {
+    const response = await fetch("localhost:3000/new-entry", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -113,11 +246,7 @@ export default defineComponent({
       type: Boolean,
       default: false
     },
-    ligandName: {
-      type: String,
-      default:''
-    },
-    formula: {
+    metalName: {
       type: String,
       default: ''
     },
@@ -125,46 +254,134 @@ export default defineComponent({
       type: String,
       default: ''
     },
-    ligandCharge: {
+    ligandName: {
+      type: String,
+      default:''
+    },
+    ligandFormula: {
+      type: String,
+      default: ''
+    },
+    fileName: {
+      type: String,
+      default: ''
+    },
+    protonationLevel: {
+      type: String,
+      default: ''
+    },
+    constantType: {
+      type: String,
+      default: ''
+    },
+    equilibriumExpression: {
+      type: String,
+      default: ''
+    },
+    temperature: {
+      type: String,
+      default: ''
+    },
+    ionicStrength: {
+      type: String,
+      default: ''
+    },
+    footnote: {
+      type: String,
+      default: ''
+    },
+    value: {
+      type: String,
+      default: ''
+    },
+    entryCode: {
+      type: String,
+      default: ''
+    },
+    DOI: {
       type: String,
       default: ''
     }
   },
   data: () => ({
-    ligandNameValue: '',
-    formulaValue: '',
+    metalNameValue: '',
     metalChargeValue: '',
-    ligandChargeValue: ''
+    ligandNameValue: '',
+    ligandFormulaValue: '',
+    ligandFileValue: '',
+    protonationLevelValue: '',
+    constantTypeValue: '',
+    equilibriumExpressionValue: '',
+    temperatureValue: '',
+    ionicStrengthValue: '',
+    footnoteValue: '',
+    valueValue: '',
+    entryCodeValue: '',
+    DOIValue: ''
   }),
   methods: {
-    onLigandNameUpdate(){
-      this.$emit('update:ligandName', this.ligandNameValue);
+    onMetalNameUpdate() {
+      this.$emit('update:metalName', this.metalNameValue);
     },
-    
-    onFormulaUpdate() {
-      this.$emit('update:formula', this.formulaValue);
-    },
-
     onMetalChargeUpdate() {
       this.$emit('update:metalCharge', this.metalChargeValue);
     },
-
-    onLigandChargeUpdate() {
-      this.$emit('update:ligandCharge', this.ligandChargeValue);
+    onLigandNameUpdate(){
+      this.$emit('update:ligandName', this.ligandNameValue);
+    },
+    onLigandFormulaUpdate() {
+      this.$emit('update:ligandFormula', this.ligandFormulaValue);
+    },
+    onLigandFileUpdate() {
+      this.$emit('update:fileName', this.ligandFileValue);
+    },
+    onProtonationLevelUpdate() {
+      this.$emit('update:protonationLevel', this.protonationLevelValue);
+    },
+    onConstantTypeUpdate() {
+      this.$emit('update:constantType', this.constantTypeValue);
+    },
+    onEquilibriumExpressionUpdate() {
+      this.$emit('update:equilibriumExpression', this.equilibriumExpressionValue);
+    },
+    onTemperatureUpdate() {
+      this.$emit('udpate:temperature', this.temperatureValue);
+    },
+    onIonicStrengthUpdate() {
+      this.$emit('update:ionicStrenth', this.ionicStrengthValue);
+    },
+    onFootnoteUpdate() {
+      this.$emit('update:footnote', this.footnoteValue);
+    },
+    onValueUpdate() {
+      this.$emit('update:value', this.valueValue);
+    },
+    onEntryCodeUpdate() {
+      this.$emit('update:entryCode', this.entryCodeValue);
+    },
+    onDOIupdate() {
+      this.$emit('update:DOI', this.DOIValue);
     },
     submitForm() {
-      
-      console.log("ligand name: " + this.ligandNameValue);
-      console.log("formula: " + this.formulaValue);
-      console.log("metalChargeValue: " + this.metalChargeValue);
-      console.log("ligand charge: " + this.ligandChargeValue);
 
       const data: dataEntry = {
-        ligandName: this.ligandNameValue,
-        formula: this.formulaValue,
+        metalName: this.metalNameValue,
         metalCharge: this.metalChargeValue,
-        ligandCharge: this.ligandChargeValue
+        ligandName: this.ligandNameValue,
+        ligandFormua: this.ligandFormulaValue,
+        ligandFile: this.ligandFileValue,
+        protonationLevel: this.protonationLevelValue,
+        constantType: this.constantTypeValue,
+        equilibriumExpression: this.equilibriumExpressionValue,
+        temperature: this.temperatureValue,
+        ionicStrength: this.ionicStrengthValue,
+        footnote: this.footnoteValue,
+        value: this.valueValue,
+        referenceEntryCode: this.entryCodeValue,
+        referenceDOIValue: this.DOIValue
       };
+
+      console.log("data: " + JSON.stringify(data))
 
       postJSON(data);
     }
