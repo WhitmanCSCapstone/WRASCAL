92a55897a82e1d846eaca6aaa74d02a73fe56ce2
Zurain Zeeshan <zurain2001@gmail.com>
2024-03-07 15:59:21 -0800

    .vue files created but need to be modified

---
+++ src/components/DataEntry/ConstantsInfo.vue
@@ -0,0 +1,27 @@
+<template>
+    <h2>Constants Information</h2>
+    <!-- Add your constants information fields here -->
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  
+  export default defineComponent ({
+      name: "ConstantsInfo",
+      props: {
+          isLoading: {
+              type: Boolean,
+              default: false
+          }
+      },
+      data: () => ({
+          // Define your constants information properties here
+      }),
+      methods: { 
+          entry(fieldToChange: String, dataToSend: any) {
+              this.$emit('entry', {'fieldToChange': fieldToChange, 'dataToSend': dataToSend})
+          }
+      }
+  })
+  </script>
+  
---
+++ src/components/DataEntry/UncertaintiesInfo.vue
@@ -0,0 +1,26 @@
+<template>
+    <h2>Uncertainties Information</h2>
+    <!-- Add your uncertainties information fields here -->
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  
+  export default defineComponent ({
+      name: "UncertaintiesInfo",
+      props: {
+          isLoading: {
+              type: Boolean,
+              default: false
+          }
+      },
+      data: () => ({
+          // Define your uncertainties information properties here
+      }),
+      methods: { 
+          entry(fieldToChange: String, dataToSend: any) {
+              this.$emit('entry', {'fieldToChange': fieldToChange, 'dataToSend': dataToSend})
+          }
+      }
+  })
+  </script>
